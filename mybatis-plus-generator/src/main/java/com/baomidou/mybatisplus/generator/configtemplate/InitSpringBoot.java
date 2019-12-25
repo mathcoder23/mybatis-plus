100a0e649e6919dba25bc4dd70bafde4219097e5
fengwei <fengwei1>
2019-12-25 21:35:54 +0800

    InitSpringBoot取消注释后使用。

--- mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/configtemplate/InitSpringBoot.java
+++ mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/configtemplate/InitSpringBoot.java
@@ -1,21 +1,22 @@
-package com.baomidou.mybatisplus.generator.configtemplate;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-
-@Component
-@Order(1) // 通过order值的大小来决定启动的顺序
-public class InitSpringBoot implements CommandLineRunner {
-
-	@Resource
-	SqlSessionFactory sqlSessionFactory;
-	@Override
-	public void run(String... args) throws Exception {
-		sqlSessionFactory.getConfiguration().getTypeHandlerRegistry().setDefaultEnumTypeHandler(BaseEnumTypeHandler.class);
-	}
-
-}
+//请取消助注释，放入代码中。
+//package com.baomidou.mybatisplus.generator.configtemplate;
+//
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.Resource;
+//
+//@Component
+//@Order(1) // 通过order值的大小来决定启动的顺序
+//public class InitSpringBoot implements CommandLineRunner {
+//
+//	@Resource
+//	SqlSessionFactory sqlSessionFactory;
+//	@Override
+//	public void run(String... args) throws Exception {
+//		sqlSessionFactory.getConfiguration().getTypeHandlerRegistry().setDefaultEnumTypeHandler(BaseEnumTypeHandler.class);
+//	}
+//
+//}
